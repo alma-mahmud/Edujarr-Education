d2f41decd75b1ecb140f07fe69736b0d9ec881f2
alma-mahmud <alma.mahmud.dev@gmail.com>
2026-02-06 19:23:58 +0600

    our-course

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navber from './components/Navber/Navber'
 import Banner from './components/Banner/Banner'
 import FeaturedCourse from './components/FeaturedCourse/FeaturedCourse'
+import OurCourses from './components/ourCourses/ourCourses'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <Navber></Navber>
     <Banner></Banner>
     <FeaturedCourse></FeaturedCourse>
+    <OurCourses></OurCourses>
     </>
   )
 }
---
+++ src/components/OurCourses/OurCourses.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import backgroud from '../../assets/Images/OurCourse/Course-bg.png'
+import banner from '../../assets/Images/OurCourse/banner7.png'
+
+function OurCourses() {
+  return (
+    <>
+    <section className='bg-linear-to-br from-[#084FC7] to-[#7F56D9] pt-20 pb-36'>
+        <div className='flex flex-col justify-center items-center'>
+            <h3 className='text-[45px] text-[#FFFFFF] font-rowdies font-bold leading-12.5 text-center pb-7.75'>Why <span className='text-[#4BE5CA]'>learn</span> with our courses?</h3>
+            <p className='text-[25px] text-[#FFFFFF] leading-8.75 font-normal font-saira text-center w-182'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporidunt ut labore veniam...</p>
+        </div>
+    </section>
+    </>
+  )
+}
+
+export default OurCourses
